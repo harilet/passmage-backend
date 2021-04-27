08969b8da489dca228eb83757d529cd7d0f93620
Harikrishnan R <mymaillet2.0@gmail.com>
2021-04-27 20:42:26 +0530

    cors fixed

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path=require('path');
 var bodyParser = require('body-parser');
 const fs = require("fs");
 
+const cors=require('cors')
+
+app.use(cors({origin:'*'}))
+
 const PORT=process.env.PORT||5000;
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -19,13 +23,18 @@ app.post('/set_encr',function (req,res) {
     
     var sers=req.body.name;
     var pas=req.body.pass_encr;
+
+    console.log(sers)
+    console.log(pas)
+
     fs.readFile('./src/encr_pass.json', function (err, data) {
         if(data.length<=2){
             data='{"'+sers+'":"'+pas+'"}';
         }
+
         data = data.slice(0, -1);
         data=data+',"'+sers+'":"'+pas+'"}';
-    
+
         fs.writeFile("./src/encr_pass.json", data,function (err) {
             console.log(err);
         });
@@ -36,6 +45,7 @@ app.post('/set_encr',function (req,res) {
 app.get('/',function (req,res) {
     fs.readFile("./src/encr_pass.json",function (err,data) {
         var json=JSON.parse(data);
+        console.log(json)
         var key=[];
         for(var a in json ){
                 key.push(a);  
